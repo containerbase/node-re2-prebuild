e075a66c20f6cfc3414a8f8e6bd2200c7e1f07ab
Michael Kriese <michael.kriese@visualon.de>
2023-12-04 11:25:22 +0100

    test: run tests on bullseye

--- bin/build.sh
+++ bin/build.sh
@@ -9,6 +9,8 @@ fi
 
 farch=x64
 darch=linux/amd64
+# glibc v2.31 (like Ubuntu 20.04)
+nodeDist=bullseye
 # TODO: set mirror
 
 if [[ "$ARCH" = "aarch64" ]]; then
@@ -23,12 +25,12 @@ echo "Installing re2 v${VERSION} for Node v${NODE_VERSION} (${farch})"
 npm install "re2@${VERSION}" --save-exact --no-audit --no-fund --prefix .cache --no-progress --platform-arch=${farch} --arch=${farch}
 
 echo "Testing re2 v${VERSION} for Node v${NODE_VERSION} (${farch})"
-docker pull --platform ${darch} "node:${NODE_VERSION}" > /dev/null 2>&1
+docker pull --platform ${darch} "node:${NODE_VERSION}-${nodeDist}" > /dev/null 2>&1
 docker run --rm \
   --platform ${darch} \
   -v "$(pwd)/.cache:/cache" \
   -w /cache \
-  "node:${NODE_VERSION}" \
+  "node:${NODE_VERSION}-${nodeDist}" \
   node -e "new require('re2')('.*').exec('test') && console.log(process.arch)"
 
 echo "Compressing re2 v${VERSION} for Node v${NODE_VERSION} (${farch})"
